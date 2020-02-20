608a602d537955ebaff6127c483d2e5e1da0b72e
Jefferson Lint <jeffersonlint@gmail.com>
2020-02-19 20:53:17 -0600

    alkjdf;ljasdlf

--- lc3bsim2.c
+++ lc3bsim2.c
@@ -495,6 +495,7 @@ void process_instruction(){
    }
    else if(opcode==2) //LDB
    {
+      int dr = (byte1>>1)&7;
       int baseR = (byte2>>6)&7;
       if(byte1&1==1) sr1=sr1+4;
       int offset6 = byte2&63;
