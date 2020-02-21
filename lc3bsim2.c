5d5c044aed80240434998fe07790737d67688830
Michael O'Leary <michael.s.oleary99@gmail.com>
2020-02-21 15:17:44 -0600

    Update lc3bsim2.c

--- lc3bsim2.c
+++ lc3bsim2.c
@@ -496,7 +496,7 @@ void process_instruction(){
    else if(opcode==2) //LDB
    {
       int dr = (byte1>>1)&7;
-      int baseR = (byte2>>6)&7;
+      int baseR = (byte2>>6)&3;
       if(byte1&1==1) baseR=baseR+4;
       int offset6 = byte2&63;
       if((offset6>>5)&1==1){  //To ensure correct sign extension
@@ -525,7 +525,7 @@ void process_instruction(){
    else if(opcode==3) //STB
    {
       int sr = (byte1>>1)&7;
-      int baseR = (byte2>>6)&7;
+      int baseR = (byte2>>6)&3;
       if(byte1&1==1) baseR=baseR+4;
       int offset6 = byte2&63;
       if((offset6>>5)&1==1){  //To ensure correct sign extension
